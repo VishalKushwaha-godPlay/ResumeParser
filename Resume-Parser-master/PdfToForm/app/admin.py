2e33e52f744debbb3823515047ea18906ac97fc1
VishalKushwaha-godPlay <68867722+VishalKushwaha-godPlay@users.noreply.github.com>
2022-02-25 22:28:40 +0530

    Add files via upload

---
+++ Resume-Parser-master/PdfToForm/app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(add_degree)
+admin.site.register(add_field_of_study)
+admin.site.register(add_skill)
+admin.site.register(add_project)
+admin.site.register(add_certifications)
+admin.site.register(social)
+admin.site.register(professional_pro)
+admin.site.register(academic)
